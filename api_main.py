f39771ad8fd2f2feeb4e1a80faedfaa9965248e1
ChristianCS351 <cookce14@mycu.concord.edu>
2025-10-23 09:56:57 -0400

    Update api_main.py
    Added some various routes to other functions

--- api_main.py
+++ api_main.py
@@ -7,8 +7,32 @@ app = Flask(__name__)
 def add_entry():
     data = request.args.get('pass')
     database_functions.add_entry(data)
-    return "success"
+    return "Successfully Added!"
 
+@app.route("/credentials/remove")
+def remove_entry():
+    data = request.args.get('pass')
+    database_functions.remove_entry(data)
+    return "Successfully Removed! "
+
+
+@app.route("/credentials/update")
+def update_entry():
+    data = request.args.get('pass')
+    database_functions.update_entry(data)
+    return "Successfully Updated!"
+
+@app.route("/credentials/get")
+def get_entry():
+    data = request.args.get('pass')
+    database_functions.get_entry(data)
+    return "Successfully Found!"
+
+@app.route("/credentials/getall")
+def get_all_entries():
+    data = request.args.get('pass')
+    database_functions.get_all_entries(data)
+    return "Successfully Displayed All!"
 
 if __name__ == "__main__":
     app.run()
