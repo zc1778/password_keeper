e04fea458c1bfd854fae2ce5592a8aa44ad26731
ChristianCS351 <cookce14@mycu.concord.edu>
2025-10-25 20:53:09 -0400

    Created master database.

--- api_main.py
+++ api_main.py
@@ -9,30 +9,30 @@ def add_entry():
     database_functions.add_entry(data)
     return "Successfully Added!"
 
-@app.route("/credentials/remove")
-def remove_entry():
-    data = request.args.get('pass')
-    database_functions.remove_entry(data)
-    return "Successfully Removed! "
+# @app.route("/credentials/remove")
+# def remove_entry():
+#     data = request.args.get('pass')
+#     database_functions.remove_entry(data)
+#     return "Successfully Removed! "
 
 
-@app.route("/credentials/update")
-def update_entry():
-    data = request.args.get('pass')
-    database_functions.update_entry(data)
-    return "Successfully Updated!"
+# @app.route("/credentials/update")
+# def update_entry():
+#     data = request.args.get('pass')
+#     database_functions.update_entry(data)
+#     return "Successfully Updated!"
 
-@app.route("/credentials/get")
-def get_entry():
-    data = request.args.get('pass')
-    database_functions.get_entry(data)
-    return "Successfully Found!"
+# @app.route("/credentials/get")
+# def get_entry():
+#     data = request.args.get('pass')
+#     database_functions.get_entry(data)
+#     return "Successfully Found!"
 
-@app.route("/credentials/getall")
-def get_all_entries():
-    data = request.args.get('')
-    database_functions.get_all_entries(data)
-    return "Successfully Displayed All!"
+# @app.route("/credentials/getall")
+# def get_all_entries():
+#     data = request.args.get('')
+#     database_functions.get_all_entries(data)
+#     return "Successfully Displayed All!"
 
 if __name__ == "__main__":
-    app.run()
+     app.run()
